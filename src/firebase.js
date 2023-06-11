cc0e7d148442d446da6461466b66a1c156257052
Naomikho <khoyining9@gmail.com>
2023-06-11 10:01:06 +0800

    set up firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,15 @@
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
  // Your web app's Firebase configuration
- const firebaseConfig = {
-   apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-   authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-   projectId: "warkop-hau-app",
-   storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXX",
- 
-   messagingSenderId: "XXXXXXXXXXXXXXX",
-   appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
- };
+const firebaseConfig = {
+  apiKey: "AIzaSyCWUdZvNslGB8D0S_5SAwfGfdGJD0m5X5c",
+  authDomain: "warkop-hau-app.firebaseapp.com",
+  projectId: "warkop-hau-app",
+  storageBucket: "warkop-hau-app.appspot.com",
+  messagingSenderId: "307075684990",
+  appId: "1:307075684990:web:459cd5eda9ef62c6e7562f",
+  measurementId: "G-R215ND2R04"
+};
  // Initialize Firebase
  
  const app = initializeApp(firebaseConfig);
