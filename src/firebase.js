dc0783e8feb0e064e061416042f0f6638059f8e9
Naomikho <khoyining9@gmail.com>
2023-07-16 20:39:47 +0800

    move firebase config to .env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
  // Import the functions you need from the SDKs you need
  import { initializeApp } from "@firebase/app";
  import { getFirestore } from "@firebase/firestore";
+ require('dotenv').config();
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
  // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCWUdZvNslGB8D0S_5SAwfGfdGJD0m5X5c",
-  authDomain: "warkop-hau-app.firebaseapp.com",
-  projectId: "warkop-hau-app",
-  storageBucket: "warkop-hau-app.appspot.com",
-  messagingSenderId: "307075684990",
-  appId: "1:307075684990:web:459cd5eda9ef62c6e7562f",
-  measurementId: "G-R215ND2R04"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
  // Initialize Firebase
  
