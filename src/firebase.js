8c1e12ad2c15e0983e158d20235762f93003ce23
Edberaga <edbetjonnathan@gmail.com>
2023-08-19 23:53:48 +0700

    Re-add the api key (for a while)

--- src/firebase.js
+++ src/firebase.js
@@ -2,22 +2,21 @@
  import { initializeApp } from "firebase/app";
  import { getFirestore } from "firebase/firestore"
  import { getAuth } from "firebase/auth";
- require('dotenv').config();
 
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
  // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+ const firebaseConfig = {
+  apiKey: "AIzaSyCWUdZvNslGB8D0S_5SAwfGfdGJD0m5X5c",
+  authDomain: "warkop-hau-app.firebaseapp.com",
+  projectId: "warkop-hau-app",
+  storageBucket: "warkop-hau-app.appspot.com",
+  messagingSenderId: "307075684990",
+  appId: "1:307075684990:web:459cd5eda9ef62c6e7562f",
+  measurementId: "G-R215ND2R04"
 };
+
  // Initialize Firebase
- 
  const app = initializeApp(firebaseConfig);
 
  //To use the firebase authentication
