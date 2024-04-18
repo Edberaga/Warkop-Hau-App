753528cfb251b17722bfa03c96fc48fcb9728767
Edberaga <edbetjonnathan@gmail.com>
2024-04-18 23:40:06 +0700

    I dont even need this...

--- src/tests/ReadData.test.js
+++
@@ -1,7 +0,0 @@
-import { getMenuItemsFromCollection } from "../services/firestore/ReadData";
-
-test('Able to Fetch Data from Firestore', async() => {
-  
-    const sayuranData = await getMenuItemsFromCollection('Sayuran');
-    expect(sayuranData).not.toBeNull();
-  });
