7abb49305d04a381a6b8d71812d9f89503ae6f8a
Edberaga <edbetjonnathan@gmail.com>
2024-04-22 00:19:55 +0700

    give class name and another sample

--- src/scenes/Admin/Storage.jsx
+++ src/scenes/Admin/Storage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import './index.css'
 
 const Storage = () => {
   const status = ['Cukup', 'Setengah', 'Sedikit', 'Habis']
@@ -16,11 +17,25 @@ const Storage = () => {
   }
 
   return (
-    <section>
+    <section className='storage'>
       <h2>Perlengkapan</h2>
-      <div>
+      <div className='list-item'>
         <p>Kertas</p>
-        <button 
+        <button
+        className='list-button'
+        style={{
+          backgroundColor:`${statusCol[count]}`
+        }}
+        onClick={setStatus}
+        >
+          {status[count]}
+        </button>
+      </div>
+
+      <div className='list-item'>
+        <p>Tusuk Gigi</p>
+        <button
+        className='list-button'
         style={{
           backgroundColor:`${statusCol[count]}`
         }}
