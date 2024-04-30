f5d79aac4fbaabf5678e42d6ac8226f0fd30509a
Edberaga <edbetjonnathan@gmail.com>
2024-04-30 23:45:48 +0700

    Retrieve the firestore data

--- src/scenes/Admin/Storage.jsx
+++ src/scenes/Admin/Storage.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { collection, getDocs } from "firebase/firestore";
 import './index.css'
+import { db } from '../../firebase'
 
 const Storage = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      let list = [];
+      try{
+        const q = collection(db, "Supplies")
+        const querySnapshot = await getDocs(q);
+
+        querySnapshot.forEach((doc) => {
+          list.push({ id:doc.id, ...doc.data()});
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+        });
+        setData(list);
+      }
+      catch(error){
+        console.log(error);
+      }
+    };
+    fetchData();
+  },[]);
+
+  console.log(data);
+
   const status = ['Cukup', 'Setengah', 'Sedikit', 'Habis']
   const statusCol = ['green', 'orange', 'red', 'gray']
   const [count, setCount] = useState(0);
